fa9b655fa7f7c19553aa0589e74f3adf10407b2a
michela <michelab299@hotmail.it>
2022-07-30 12:18:05 +0100

    implement posts and users

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params) # fills in user_id in the new post. current_user = from devise
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :posts, only: [:index, :new, :create]
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
 end
