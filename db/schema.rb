9f4849779d2a97ded6ac47999d078b187c0f44a9
michela <michelab299@hotmail.it>
2022-07-30 17:41:16 +0100

    add username and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_30_095223) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_30_160016) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_30_095223) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
