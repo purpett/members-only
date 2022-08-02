d875e827abd97a604d44da50b426f049599f446a
michela <michelab299@hotmail.it>
2022-08-02 19:12:51 +0100

    style posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index, :new, :create]
+  resources :posts
   # Defines the root path route ("/")
   root "posts#index"
 end
