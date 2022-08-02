b0106e905dcdef3946aa97fbc8ad2ba50ae565f1
michela <michelab299@hotmail.it>
2022-08-02 19:25:08 +0100

    edit style posts#index with flex

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc)
   end
 
   def new
