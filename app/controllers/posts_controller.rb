d8091308b93833b55ec9b9c4842c534f8b3d6337
michela <michelab299@hotmail.it>
2022-08-12 14:26:14 +0100

    add view and controller for posts#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,10 @@ class PostsController < ApplicationController
     @posts = Post.order(created_at: :desc)
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
   end
