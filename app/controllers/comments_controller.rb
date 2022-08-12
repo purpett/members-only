c7d5646150a60158f7912a476099604514972225
michela <michelab299@hotmail.it>
2022-08-12 18:01:33 +0100

    implement comment controller and view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,36 @@
 class CommentsController < ApplicationController
-  def index
-    
-  end
-  
   def new
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build   # associazione post caricata. Build = new ma su associazione. Build non salva
   end
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else 
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-  end
+  # def update
+  # end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@comment.post) # navigates back to post - whole point of associations (belongs_to)
   end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
 end
